c44a59d3a28a8f91107975bea2fe5dfaa3434618
Alexandra A. de Sousa <38778376+AleAliSousa@users.noreply.github.com>
2024-05-23 17:32:08 -0700

    restored from Sharepoint trash - sorry!
    I was having trouble getting the new files from Miloni for Powell and MacLarnon into the main branch and ended up restoring them from SharPoint trash. Very sorry.

---
+++ Powell_etal_2017/Powell_etal_2017_Dataset1.R
@@ -0,0 +1,40 @@
+#1. Source
+setwd("C:/Users/MILONI/OneDrive - University of Bath/Research Schemes/Allen Institue/Evo-M1-Trait-Data/")
+
+#2. Table 1
+
+## 1. Read direct from xl
+library(readxl)
+folder_path <- "./Powell_etal_2017/"
+tabledirectxl <- read_excel(paste0(folder_path,"Powell_etal_2017_Dataset1_snapshot.xlsx"))
+
+## 2. Convert columns to numerical
+tabledirectxl$`Terrestriality` <- as.numeric(tabledirectxl$`Terrestriality`)
+tabledirectxl$`Sleeping group size` <- as.numeric(tabledirectxl$`Sleeping group size`)
+tabledirectxl$`HR range` <- as.numeric(tabledirectxl$`HR range`)
+tabledirectxl$`Source Home range size` <- as.numeric(tabledirectxl$`Source Home range size`)
+
+#3. Save
+
+# Finalize dataframe (UPDATE!!!)
+final.dataframe <- tabledirectxl
+
+# Get Item name: Get Path of the current script, Extract the file name, Remove the ".R" extension
+library(rstudioapi)
+item_name <- gsub("\\.R$", "", basename(rstudioapi::getActiveDocumentContext()$path))
+
+# Get Item encoded
+library(readxl) 
+filecodes <- read_excel("./__ReadMe.xlsx", sheet = "Sheet1")
+item_encoded <- filecodes$"Item encoded"[match(item_name, filecodes$"Item name")]
+
+# Save dataframe to a CSV file
+write.csv(final.dataframe, file = paste0(folder_path, item_name, ".csv"), row.names = FALSE)
+
+# Save dataframe to a TSV file in the online database
+tsv_file_path <- "./__Public/comparative-data/"
+write.table(final.dataframe, file = paste0(tsv_file_path, item_encoded, ".tsv"), sep = "\t", row.names = FALSE)
+a
+
+# Day range - 96 - convert from km to m? But what to do with f and m? 
+# the convertion works but got a warning saying - NAs introduced by coercion
